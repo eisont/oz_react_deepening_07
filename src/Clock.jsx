f2d6af481ced2eb6af7eefad2ee3da24b459fcd0
치훈 <rlaclgns321@naver.com>
2025-08-13 14:46:29 +0900

    실시간 시계 어플리케이션 제작하기_완료

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 /**
  * Clock 컴포넌트
  *
@@ -8,8 +10,59 @@
  * - 현재 시간을 "HH:mm:ss" 형식으로 표시합니다.
  * - 시계가 실행 중일 때 매초마다 시간을 업데이트합니다.
  **/
-function Clock() {
-  return <div className="timer-container"></div>;
-}
+const Clock = () => {
+  const [time, setTime] = useState('');
+  const [toggleBt, setToggleBt] = useState(true);
+  const [intervalId, setIntervalId] = useState(null);
+
+  const CurrentTime = () => {
+    const currentDate = new Date();
+    const hour = currentDate.getHours();
+    const minutes = currentDate.getMinutes();
+    const seconds = currentDate.getSeconds();
+
+    return setTime(`${hour} : ${minutes} : ${seconds}`);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      CurrentTime();
+    }, 1000);
+
+    setIntervalId(interval);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const timerStart = () => {
+    if (!intervalId) {
+      const id = setInterval(() => CurrentTime(), 1000);
+      setIntervalId(id);
+      setToggleBt(true);
+    }
+  };
+  const timerStop = () => {
+    if (intervalId) {
+      clearInterval(intervalId);
+      setIntervalId(null);
+      setToggleBt(false);
+    }
+  };
+
+  return (
+    <div className="timer-container">
+      <div>{time}</div>
+      {toggleBt ? (
+        <button onClick={timerStop} className="stop bt">
+          타이머 정지
+        </button>
+      ) : (
+        <button onClick={timerStart} className="start bt">
+          타이머 시작
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default Clock;
